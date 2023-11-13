dc03311f00e08b2c3736bb6f3eca37356416547a
crea8ture c <kai@thebodyliterate.com>
2023-11-13 14:45:12 -0500

    new

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ export default function Header(){
     return(
             <header className="layout-header" style={{fontFamily: "monospace"}}> 
          <h1>{"<h1>"}code deets{"</h1>"}</h1> 
-         <h5>{'<h5>'}code deets like twitter for code. 250 char html, css, js snippets {' </h5>'}</h5>
+         <h5>{'<h5> '}code deets like twitter for code. 250 char html, css, js snippets {' </h5>'}</h5>
 
        </header> 
     )
